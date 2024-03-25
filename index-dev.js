ff6c721a6a9f5a8f253ee8611ff90d012a358368
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-03-25 11:48:57 +0100

    Release 1.4.4-compact-left-v2

--- index-dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = require("./lib/ReactGridLayout").default;
-module.exports.utils = require("./lib/utils");
-module.exports.calculateUtils = require("./lib/calculateUtils");
-module.exports.Responsive = require("./lib/ResponsiveReactGridLayout").default;
-module.exports.Responsive.utils = require("./lib/responsiveUtils");
-module.exports.WidthProvider =
-  require("./lib/components/WidthProvider").default;
